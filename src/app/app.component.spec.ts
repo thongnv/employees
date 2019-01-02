d341655cff9814a818056e668d107771177f31c3
Nguyen Van Thong (DTL.Mail) <ThongNV1@fsoft.com.vn>
2019-01-02 17:01:50 +0700

    add Heroes comopnent

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'hero-app'`, () => {
+  it(`should have as title 'This is an awesome app!'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('hero-app');
+    expect(app.title).toEqual('This is an awesome app!');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to hero-app!');
+    expect(compiled.querySelector('h1').textContent).toEqual('This is an awesome app!');
   });
 });
